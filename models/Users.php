e5aef046e324459df8ac40c99c3b001fd57c0dab
SoB1nary <rbs.mine.27@gmail.com>
2024-02-02 23:03:24 +0600

    among us

--- models/Users.php
+++ models/Users.php
@@ -70,6 +70,9 @@ class Users extends \yii\db\ActiveRecord implements IdentityInterface
         return static::findOne($id);
     }
 
+    /**
+     * @throws NotSupportedException
+     */
     public static function findIdentityByAccessToken($token, $type = null)
     {
       throw new NotSupportedException();
@@ -104,4 +107,55 @@ class Users extends \yii\db\ActiveRecord implements IdentityInterface
             }
         }
     }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(string $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(string $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
 }
