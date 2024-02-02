72168d558a0a0cc529afd0cb06240abacbb034f9
SoB1nary <rbs.mine.27@gmail.com>
2024-02-02 22:37:38 +0600

    among us

--- models/Users.php
+++ models/Users.php
@@ -3,6 +3,9 @@
 namespace app\models;
 
 use Yii;
+use yii\base\Exception;
+use yii\base\NotSupportedException;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "users".
@@ -15,12 +18,12 @@ use Yii;
  * @property string $created_at
  * @property string $updated_at
  */
-class Users extends \yii\db\ActiveRecord
+class Users extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
      */
-    public static function tableName()
+    public static function tableName(): string
     {
         return 'users';
     }
@@ -28,7 +31,7 @@ class Users extends \yii\db\ActiveRecord
     /**
      * {@inheritdoc}
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             [['username', 'password', 'auth_key', 'email', 'created_at', 'updated_at'], 'required'],
@@ -40,7 +43,7 @@ class Users extends \yii\db\ActiveRecord
     /**
      * {@inheritdoc}
      */
-    public function attributeLabels()
+    public function attributeLabels(): array
     {
         return [
             'id' => Yii::t('app', 'ID'),
@@ -57,8 +60,48 @@ class Users extends \yii\db\ActiveRecord
      * {@inheritdoc}
      * @return UsersQuery the active query used by this AR class.
      */
-    public static function find()
+    public static function find(): UsersQuery
     {
         return new UsersQuery(get_called_class());
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+      throw new NotSupportedException();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey(): string
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey): bool
+    {
+        return $this->getAuthKey()===$authKey;
+    }
+    public function validatePassword(string $password): bool
+    {
+        return Yii::$app->security->validatePassword($password, $this->password);
+    }
+    /**
+     * @throws Exception
+     */
+    public function beforeSave($insert)
+    {
+        if(parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->auth_key = Yii::$app->security->generateRandomString();
+            }
+        }
     }
 }
